198e7ffeaf8414ad681343f6a216bf6663c554df
annisaa <annisaagustina84@gmail.com>
2025-03-14 20:12:29 +0700

    Jobsheet 3-Praktikum 2

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
-
-public function index()
 {
-    return view('sales.index');
+public function index(){
+
+    return view('sales');
+}
 }
