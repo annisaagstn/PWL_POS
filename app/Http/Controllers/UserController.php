0518a4ed4c4c14c13efb6697b98b5f4f63749a41
annisaa <annisaagustina84@gmail.com>
2025-03-15 16:51:16 +0700

    Jobsheet 3-Praktikum 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
     
     class UserController extends Controller
     {
-    public function show($id, $name)
+    public function index()
     {
-        return view('user, show')
-        ->with('name', $name)
-        ->with('id', $id);
+        //tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+        
+        //coba akses model UserModel
+        $users = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $users]);
     }
     }
