d9f4f29d6722c00e65d73ca16e16d5e595eb91ec
annisaa <annisaagustina84@gmail.com>
2025-03-16 16:02:02 +0700

    Jobsheet 4-Praktikum 2.2-Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,15 +28,6 @@
          // ];
          // UserModel::where('username', 'customer-1')->update($data);
  
-         $data = [
-             'level_id' => 2,
-             'username' => 'manager_tiga',
-             'nama' => 'Manager 3',
-             'password' => Hash::make('12345'),
-         ];
-         UserModel::create($data);
-         // // Coba akses model UserModel
-         $user = UserModel::all();
          // $data = [
          //     'level_id' => 2,
          //     'username' => 'manager_tiga',
@@ -62,9 +53,19 @@
          // });
          // return view('user', ['data' => $user]);
  
-         $user = UserModel::findOr(20, ['username', 'nama'], function () {
-             abort(404);
-         });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+      //   });
+         // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+         //     abort(404);
+         // });
+         // return view('user', ['data' => $user]);
+ 
+         //$user = UserModel::findOrFail(1);
+         //return view('user', ['data' => $user]);
+
+         $user = UserModel::where('username', 'manager9')->firstOrFail();
          return view('user', ['data' => $user]);
+ 
      }
  }
