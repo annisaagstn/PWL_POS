3367527b82ffd2f53e3c11b0a796a3efea45e693
annisaa <annisaagustina84@gmail.com>
2025-03-16 16:38:30 +0700

    Jobsheet 4-Praktikum 2.3-Retrieving Aggregates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,8 +64,15 @@
          //$user = UserModel::findOrFail(1);
          //return view('user', ['data' => $user]);
 
-         $user = UserModel::where('username', 'manager9')->firstOrFail();
+         //$user = UserModel::where('username', 'manager9')->firstOrFail();
+         //return view('user', ['data' => $user]);
+
+         //$user= UserModel::where('level_id', 2)->count();
+         //dd($user);
+         //return view('user', ['data' => $user]);
+
+         $user = UserModel::where('level_id', 2)->get();
+         //dd($user);
          return view('user', ['data' => $user]);
- 
      }
  }
