a6dda622c92e49cf1613e71481c770415a4ae9e1
annisaa <annisaagustina84@gmail.com>
2025-06-16 15:05:03 +0700

    Jobsheet 5 Tugas 1 - Implementasi CRUD tabel m_level

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Halaman awal
 Route::get('/', [WelcomeController::class, 'index']);
 
-// Route untuk level
-Route::get('/level', [LevelController::class, 'index']);
-
-// Route untuk kategori
 Route::get('/kategori', [KategoriController::class, 'index']);
 
-// Route untuk user
 Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']);           // Tampilkan semua user
-    Route::post('/list', [UserController::class, 'list']);       // DataTables list JSON
-    Route::get('/create', [UserController::class, 'create']);    // Form tambah user
-    Route::post('/', [UserController::class, 'store']);          // Simpan user baru
-    Route::get('/{id}', [UserController::class, 'show']);        // Detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);   // Form edit user
-    Route::put('/{id}', [UserController::class, 'update']);      // Simpan perubahan
-    Route::delete('/{id}', [UserController::class, 'destroy']);  // Hapus user
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
 });
