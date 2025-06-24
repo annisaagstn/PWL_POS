8cff0acc59174dbc64bf0a433ed579203f7b6cf0
annisaa <annisaagustina84@gmail.com>
2025-06-24 13:59:47 +0700

    Jobsheet 7 Praktikum 2 - Implementasi Authorization

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Path to redirect users after login.
+     */
+    public const HOME = '/home';
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+        });
+    }
+}
